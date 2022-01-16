35603e21ab159f318e114f408e9380604bc9d2ad
gaspno <zatchgabriel@gmail.com>
2022-01-16 11:59:07 -0300

    img add new visual css grid layout

--- 3-frontend/angular-ecommerce/src/app/services/products.service.ts
+++ 3-frontend/angular-ecommerce/src/app/services/products.service.ts
@@ -9,7 +9,7 @@ import { map } from 'rxjs/operators'
 })
 export class ProductsService {
 
-  private baseUrl='http://localhost:8080/api/products';
+  private baseUrl='http://localhost:8080/api/products?size=100';
 
   constructor(private httpClient:HttpClient ) {
 
